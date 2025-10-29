fc80a8c03822688d8df5f4a942174b93b45e9d94
jaasminenb <izzayaserere@gmail.com>
2025-10-30 02:02:14 +0700

    Modifikasi Percobaan 4

--- NilaiKelompok12.java
+++ NilaiKelompok12.java
@@ -4,7 +4,8 @@ public class NilaiKelompok12 {
         Scanner sc = new Scanner(System.in);
         int kelompok = 1;
         double totalNilai, rataNilai;
-
+        double tertinggi = 0;
+        int kelompokTertinggi = 0;
         while (kelompok <= 6) {
             totalNilai = 0;
             System.out.println("Kelompok " + kelompok);
@@ -13,12 +14,16 @@ public class NilaiKelompok12 {
                 double nilai = sc.nextDouble();
                 totalNilai += nilai;
             }
-
             rataNilai = totalNilai / 5;
             System.out.println("Rata-rata nilai kelompok " + kelompok + " = " + rataNilai);
             System.out.println();
-
+            if (rataNilai > tertinggi) {
+                tertinggi = rataNilai;
+                kelompokTertinggi = kelompok;
+            }
             kelompok++;
         }
+        System.out.println("Kelompok dengan rata-rata tertinggi adalah: Kelompok " + kelompokTertinggi);
+        System.out.println("Nilai rata-rata tertinggi: " + tertinggi);
     }
 }
